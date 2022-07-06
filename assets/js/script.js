bc4bd02602b73a4fc84d328a60b5fb75fda3beea
smandla <35283559+smandla@users.noreply.github.com>
2022-07-05 21:16:48 -0700

    Add comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/** get elements  */
 var formEl = $("#form");
 var searchInputEl = $("#search_input");
 var directionsEl = $("#initial_stmt");
@@ -14,43 +15,60 @@ var currentUvindexEl = $("#current_uvindex");
 var cityHistoryListEl = $("#city_history_list");
 var currenticonDescriptionEl = $("#icon_description");
 
+/** variables that are empty */
 var cityInput;
 var savedData;
 var val = "";
 var cityHistory = [];
+
 //moment formatting code
 var formattedDate = moment().format("dddd, MMMM Do, YYYY");
 
+/** when form is submitted */
 formEl.on("submit", function (e) {
   e.preventDefault();
+  //grabs search input val
   var searchInput = searchInputEl.val();
-  console.log(searchInput);
+  //if it's empty return
   if (searchInput === "") {
     return;
   }
+  //make cityInput = searchInput
   cityInput = searchInput;
+  //make val = cityInput
   val = cityInput;
+  //if cityInput is not in cityHistory then push it to it
   if (!cityHistory.includes(cityInput)) {
     cityHistory.push(cityInput);
   }
+  //add to history list
   showHistory(cityInput, cityHistory.indexOf(cityInput));
-  console.log(cityHistory);
+
+  //clear search val
   searchInputEl.val("");
+  //add to local storage
   localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
-  // showHistory();
+  // call getData() function
   getData();
 });
 
+/**
+ * Function that asynchronously gets the data for current and forecast
+ */
 const getData = async () => {
+  //get city response in order to get lat/long coords
   let cityToLatLonResponse = await fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
       cityInput +
       "&units=imperial&appid=71aa7cd481b37deac0ed481ce3ecd9ee"
   );
+  //get json object
   let cityData = await cityToLatLonResponse.json();
   if (cityToLatLonResponse) {
     savedData = cityData;
   }
+
+  //get current and daily data
   let cityDetailsResonse = await fetch(
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
       cityData.coord.lat +
@@ -59,11 +77,14 @@ const getData = async () => {
       "&exclude=hourly,minutely,alerts&units=imperial&appid=71aa7cd481b37deac0ed481ce3ecd9ee"
   );
   let cityDetails = await cityDetailsResonse.json();
+  //call showData() function to change data on site
   showData(cityDetails);
 };
 
+/**
+ * Function that displays the data in current section
+ */
 const showData = (cityDetails) => {
-  console.log(cityDetails);
   directionsEl.fadeOut(200, function () {
     $(this).remove();
   });
@@ -75,12 +96,12 @@ const showData = (cityDetails) => {
   currentDateEl.text(formattedDate);
   currentIconEl.attr(
     "src",
-    "http://openweathermap.org/img/wn/" +
+    "https://openweathermap.org/img/wn/" +
       cityDetails.current.weather[0].icon +
       "@2x.png"
   );
   currenticonDescriptionEl.text(cityDetails.current.weather[0].description);
-  // console.log(cityDetails.current.weather);
+
   currentTempEl.text(cityDetails.current.temp + "°");
   currentHumidityEl.text(cityDetails.current.humidity + "%");
   currentWindSpeedEl.text(cityDetails.current.wind_speed + "mph");
@@ -101,17 +122,21 @@ const showData = (cityDetails) => {
   if (uvi > 10) {
     currentUvindexEl.css("background-color", "purple");
   }
-  showForecastDetails(cityDetails.daily.slice(1, 6));
+
+  showForecastDetails(cityDetails.daily.slice(0, 5));
 };
 
+/**
+ *
+ * Function that displays the forecast details
+ */
 const showForecastDetails = (data) => {
-  console.log(data);
+  //clear forecast cards
   forecastCardsEl.text("");
+  //create cards for daily forecast details
   for (let i = 0; i < data.length; i++) {
     let sectionEl = $("<section>").addClass("card").appendTo(forecastCardsEl);
     let date = moment.unix(data[i].dt).toDate();
-
-    // console.log(date);
     let h4El = $("<h4>")
       .text(moment(date).format("MMMM Do, YYYY"))
       .appendTo(sectionEl);
@@ -131,18 +156,17 @@ const showForecastDetails = (data) => {
       .appendTo(sectionEl);
   }
 };
+/**
+ * Function that shows History list by adding li elements at a time
+ */
 const showHistory = (city, i) => {
-  console.log(cityHistory);
-
   var city = cityHistory[i];
-  console.log(city, i);
   var li = $("<li>")
     .text(city)
     .attr("id", "history-" + i)
     .on("click", function (e) {
       e.preventDefault();
       cityInput = e.target.innerHTML;
-      // console.log(val);
       getData();
     });
   li.appendTo(cityHistoryListEl);
@@ -150,19 +174,17 @@ const showHistory = (city, i) => {
 
 const init = () => {
   //get local storage items and save to variable
-  //show search history from the local storage items
   var storedData = JSON.parse(localStorage.getItem("cityHistory"));
   if (storedData !== null) {
     cityHistory = storedData;
   }
-  console.log(storedData);
+  /**Add all history items on page load from local storage data */
   for (let i = 0; i < cityHistory.length; i++) {
-    console.log("here");
     showHistory(cityHistory[i], i);
   }
-
+  //hide sections
   showDetailsEl.hide();
   forecastDetailsEl.hide();
 };
+//call init() function
 init();
-// localStorage.removeItem("cityHistory");
